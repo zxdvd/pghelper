be4c76a5650ece8fe0555f313472c989460a59fd
Xudong Zhang <zxdvd_zhang@163.com>
2019-08-28 19:33:30 +0800

    func: add text_to_array

--- sql/100_convert.sql
+++ sql/100_convert.sql
@@ -1,5 +1,5 @@
 
-create or replace function helper.to_int(s text) returns int as $$
+create or replace function helper.to_int(s anyelement) returns int as $$
 begin
         return s::int;
 exception when others then
@@ -7,10 +7,16 @@ exception when others then
 end
 $$ language plpgsql immutable;
 
-create or replace function helper.to_float(s text) returns float as $$
+create or replace function helper.to_float(s anyelement) returns float as $$
 begin
         return s::float;
 exception when others then
         return null;
 end
 $$ language plpgsql immutable;
+
+-- convert text like '[1,2,3]' to pg array
+-- select '1' = any(helper.text_to_array('[1,2,3]'))
+create or replace function helper.text_to_array(s text) returns text[] as $$
+        select array_agg(x)  from  json_array_elements_text(s::json) x;
+$$ language sql immutable;
