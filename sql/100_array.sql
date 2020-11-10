04e91fb5722396cfe772cee9be9edc07a67f035f
Xudong Zhang <zxdvd_zhang@163.com>
2020-11-10 14:42:27 +0800

    func: add unnest_with_index(arr anyarray)

--- sql/100_array.sql
+++ sql/100_array.sql
@@ -1,5 +1,10 @@
 
 -- uniq an array [1,2,3,1,1,2] to [1,2,3]
 create or replace function helper.array_uniq(arr anyarray) returns anyarray as $$
-  return select array_agg(a) from (select unnest(arr) a group by a) tmp;
+  select array_agg(a) from (select unnest(arr) a group by a) tmp;
+$$ language sql immutable strict;
+
+-- unnest an array with index ['a', 'b', 'c'] to [(1, 'a'), (2, 'b'), (3, 'c')]
+create or replace function helper.unnest_with_index(arr anyarray) returns table(key int, value anyelement)  as $$
+    select generate_series(1, array_length(arr, 1)), unnest(arr);
 $$ language sql immutable strict;
